1533831987e7ff3ff7d29563fb4d1233410171dc
Alexandre Paixao <alexandre.paixao@stadline.com>
2020-11-01 22:53:50 +0100

    prepare the chat

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,31 @@ import './App.css';
 interface AppProps {}
 
 function App({}: AppProps) {
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    alert('submit!');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>This is Chatty chat!</h2>
+      <ul className="Message-list">
+        <li className="Message-item">
+          <span className="Message-author">Bot</span> Welcome!
+        </li>
+      </ul>
+      <form className="Message-form" onSubmit={onSubmit}>
+        <input
+          className="Message-input"
+          type="text"
+          name="newMessage"
+          placeholder="Type your message"
+          autoFocus
+        />
+        <button className="Message-submit-button" type="submit">
+          Send
+        </button>
+      </form>
     </div>
   );
 }
