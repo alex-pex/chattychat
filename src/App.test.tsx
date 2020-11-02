d24fc2a82c0549707878b0f3db22700aaba0ce2b
Alexandre Paixao <alexandre.paixao@stadline.com>
2020-11-02 14:30:42 +0100

    prepare functional tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,32 +1,11 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
-import { App } from './App';
+import App from './App';
 
-describe('[unit] test App.tsx', () => {
-  test('renders learn react link', () => {
-    const { getByText, queryByText } = render(<App />);
+describe('[functional] test the real app', () => {
+  test('a', () => {
+    const { getByText, debug } = render(<App />);
     const linkElement = getByText(/This is Chatty chat!/i);
     expect(linkElement).toBeInTheDocument();
-
-    const message = queryByText(/hello/);
-    expect(message).not.toBeInTheDocument();
-  });
-
-  test('displays messages', () => {
-    const { getByText } = render(<App messages={['hello']} />);
-    const message = getByText(/hello/);
-    expect(message).toBeInTheDocument();
-  });
-
-  test('filters duplicate messages', () => {
-    const { queryAllByText } = render(
-      <App messages={['hello', 'world', 'world', 'hello']} />,
-    );
-
-    // expected: hello, world, hello
-    const helloMsgs = queryAllByText(/hello/);
-    const worldMsgs = queryAllByText(/world/);
-    expect(helloMsgs.length).toBe(2);
-    expect(worldMsgs.length).toBe(1);
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { connect } from 'react-redux';
-import type { RootState } from './store';
+import { Provider } from 'react-redux';
+import store from './store';
+import Messages from './Messages';
+import './index.css';
 
-interface AppProps {
-  messages?: string[];
-}
+const App = () => (
+  <Provider store={store}>
+    <Messages />
+  </Provider>
+);
 
-export function App({ messages = [] }: AppProps) {
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    alert('submit!');
-  };
-
-  const filteredMessages = messages.reduce((acc, msg) => {
-    if (acc.length > 0 && acc[acc.length - 1] === msg) return acc;
-    return [...acc, msg];
-  }, [] as typeof messages);
-
-  return (
-    <div className="App">
-      <h2>This is Chatty chat!</h2>
-
-      <ul className="Message-list">
-        {filteredMessages.map((msg, i) => (
-          <li key={i} className="Message-item">
-            <span className="Message-author">Message</span> {msg}
-          </li>
-        ))}
-      </ul>
-      <form className="Message-form" onSubmit={onSubmit}>
-        <input
-          className="Message-input"
-          type="text"
-          name="newMessage"
-          placeholder="Type your message"
-          autoFocus
-        />
-        <button className="Message-submit-button" type="submit">
-          Send
-        </button>
-      </form>
-    </div>
-  );
-}
-
-const mapStateToProps = (state: RootState) => ({
-  messages: state.messages,
-});
-
-export default connect(mapStateToProps)(App);
+export default App;
