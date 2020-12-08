08bbffe4bf34ad33c005b427542aa731620949e5
Alexandre Paixao <alexandre.paixao@stadline.com>
2020-12-08 10:55:13 +0100

    prepare the browser

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
+import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/This is Star Wars/i);
   expect(linkElement).toBeInTheDocument();
 });
